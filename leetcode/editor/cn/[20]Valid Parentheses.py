634b249f39c253c65f1ad2c9adb15fff7a5a7974
Mingzhu <mznie@hotmail.com>
2022-02-19 00:29:16 -0600

    blind75-p-match

---
+++ leetcode/editor/cn/[20]Valid Parentheses.py
@@ -0,0 +1,60 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']
+# ', determine if the input string is valid. 
+# 
+#  An input string is valid if: 
+# 
+#  
+#  Open brackets must be closed by the same type of brackets. 
+#  Open brackets must be closed in the correct order. 
+#  
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: s = "()"
+# Output: true
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: s = "()[]{}"
+# Output: true
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: s = "(]"
+# Output: false
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= s.length <= 10⁴ 
+#  s consists of parentheses only '()[]{}'. 
+#  
+#  Related Topics 栈 字符串 👍 2982 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack=[]
+        lookup={"(":")","[":"]","{":"}"}
+
+        for p in s:
+            if p in lookup:
+                stack.append(p)
+            elif len(stack)==0 or lookup[stack.pop()]!=p:
+                return False
+        return len(stack)==0
+# leetcode submit region end(Prohibit modification and deletion)
